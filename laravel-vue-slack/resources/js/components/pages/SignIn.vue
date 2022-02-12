6c62e92593c8ec111d7c5d8e519e29d73930ef3d
yasuaki <yasuakihappy2@yahoo.co.jp>
2022-02-12 16:38:46 +0900

    1-5：TailwindCSSをプロジェクトに導入しよう

--- laravel-vue-slack/resources/js/components/pages/SignIn.vue
+++ laravel-vue-slack/resources/js/components/pages/SignIn.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+      テスト用のボタン
+    </button>
     <p>サインイン画面</p>
     <router-link to="/chat">チャット画面へ</router-link>
   </div>
