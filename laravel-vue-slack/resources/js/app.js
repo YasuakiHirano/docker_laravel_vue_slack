2b9f9e3a34ae5ec300cc94e5f398544dfc2051fb
yasuaki <yasuakihappy2@yahoo.co.jp>
2022-02-12 18:42:32 +0900

    2-1：ヘッダーを作成しよう

--- laravel-vue-slack/resources/js/app.js
+++ laravel-vue-slack/resources/js/app.js
@@ -13,6 +13,7 @@ import HappyIcon from './components/atoms/HappyIcon.vue'
 import SignInButton from './components/atoms/SignInButton.vue'
 import SignInText from './components/atoms/SignInText.vue'
 import AccountCreateButton from './components/atoms/AccountCreateButton.vue'
+import UserInformation from './components/atoms/UserInformation.vue'
 
 app.component('form-label', FormLabel)
 app.component('form-text', FormText)
@@ -20,10 +21,14 @@ app.component('happy-icon', HappyIcon)
 app.component('sign-in-button', SignInButton)
 app.component('sign-in-text', SignInText)
 app.component('account-create-button', AccountCreateButton)
+app.component('user-information', UserInformation)
 
 // molecules
 import AppTitle from './components/molecules/AppTitle.vue'
+import ChatHeader from './components/molecules/ChatHeader.vue'
+
 app.component('app-title', AppTitle)
+app.component('chat-header', ChatHeader)
 
 // pages
 import Chat from './components/pages/Chat.vue'
--- laravel-vue-slack/resources/js/components/pages/Chat.vue
+++ laravel-vue-slack/resources/js/components/pages/Chat.vue
@@ -1,17 +1,60 @@
 <template>
-  <div>
-    Hello, {{ text }}
-    <router-link to="/">サインイン画面へ</router-link>
+  <div class="main">
+    <chat-header class="header" userName="テストユーザー" />
   </div>
 </template>
 <script>
 export default {
   setup() {
-    const text = 'World'
-
-    return {
-      text
-    }
   }
 }
 </script>
+<style scoped>
+.main {
+  display: grid;
+  grid-template-columns: minmax(210px, 20%) 1fr;
+  grid-auto-rows: 30px calc(100vh - 30px);
+  min-height: 100vh;
+  margin: 0;
+}
+.header {
+  grid-row: 1;
+  grid-column: 1 / span 2;
+  background-color:#350d36;
+}
+.side-menu {
+  grid-row: 2;
+  grid-column: 1;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  background-color:#3F0E40;
+}
+.message-area {
+  grid-row: 2;
+  grid-column: 2;
+  overflow-y: hidden;
+  overflow-x: hidden;
+
+  display: grid;
+  grid-auto-rows: 50px 1fr 100px;
+}
+@media screen and (max-width: 500px) {
+  .main {
+    grid-template-columns: 1fr;
+  }
+  .side-menu {
+    display: none;
+  }
+  .message-area {
+    grid-column: 1;
+  }
+}
+.list-enter-active,
+.list-leave-active {
+  transition: opacity 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+}
+</style>
